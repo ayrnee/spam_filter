9ac6e6ac0b684f36d593ac9f81561f4cf2f1bf39
ayrnee <ayrnee@gmail.com>
2018-05-24 14:26:03 -0400

    added timing

--- src/app.py
+++ src/app.py
@@ -1,163 +1,7 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
-85
-86
-87
-88
-89
-90
-91
-92
-93
-94
-95
-96
-97
-98
-99
-100
-101
-102
-103
-104
-105
-106
-107
-108
-109
-110
-111
-112
-113
-114
-115
-116
-117
-118
-119
-120
-121
-122
-123
-124
-125
-126
-127
-128
-129
-130
-131
-132
-133
-134
-135
-136
-137
-138
-139
-140
-141
-142
-143
-144
-145
-146
-147
-148
-149
-150
-151
-152
-153
-154
-155
-156
-157
-158
+#!/usr/bin/env python
 from collections import defaultdict
 import numpy as np
+import time
 
 def build_vocab(data):
     word_count = defaultdict(lambda : 0)
@@ -231,7 +75,7 @@ def perceptron_train_avg(data, max_itr = 100):
             w_vecs.append(w)
         itr = itr + 1
     w_vecs = np.mean(np.array(w_vecs), axis=0)
-    print w_vecs
+    # print w_vecs
 
     return w_vecs, k, itr
 
@@ -276,41 +120,71 @@ def relevant_weights(w, vocab, count):
         least_impact.append(vocab[sorted_weights[len(sorted_weights) - i - 1]])
     return most_impact, least_impact
 
+def training_data_impact(raw_data):
+    steps = [100, 200, 400, 800, 2000, 4000]
+    for step in steps:
+        print "Using N = " + str(step)
+        training, validation = partition_data(raw_data, step)
+        training = clean_data(training)
+        validation = clean_data(validation)
+        vocab = build_vocab(training[1])
+        samples = build_feature_list(training[1], vocab)
+        data = {}
+        data["train_x"] = samples
+        data["train_y"] = training[0]
+        w, k, itr = perceptron_train_avg(data, 1000)
+        print "The error on the training set using the averaged perceptron is: " + str(perceptron_test(w, data, "train_x", "train_y"))
+        validation_samples = build_feature_list(validation[1], vocab)
+        data["val_x"] = validation_samples
+        data["val_y"] = validation[0]
+        print "The error on the validation set using the averaged perceptron is: " + str(perceptron_test(w, data, "val_x", "val_y"))
+
+        w, k, itr = perceptron_train(data, 1000)
+        print "The error on the training set using the perceptron is: " + str(perceptron_test(w, data, "train_x", "train_y"))
+        validation_samples = build_feature_list(validation[1], vocab)
+        data["val_x"] = validation_samples
+        data["val_y"] = validation[0]
+        print "The error on the validation set using the perceptron is: " + str(perceptron_test(w, data, "val_x", "val_y"))
+
 
 def main():
+    tic = time.time()
     raw_data = read_data("./data/spam_train.txt")
     print "File opened"
-    training, validation = partition_data(raw_data, 4000)
-    training = clean_data(training)
-    validation = clean_data(validation)
-    print "Data pre-processed"
-    vocab = build_vocab(training[1])
-    print "Vocabulary built"
-    samples = build_feature_list(training[1], vocab)
-    print "Feature matrix formed"
-    data = {}
-    data["train_x"] = samples
-    data["train_y"] = training[0]
-    w, k, itr = perceptron_train_avg(data, 1000)
-    # print w
-    print "The number of misclasified samples: "
-    print k
-    # print itr
-    print "The error on the training set is: " + str(perceptron_test(w, data, "train_x", "train_y"))
-    validation_samples = build_feature_list(validation[1], vocab)
-    print "Validation samples matrix formed"
-    data["val_x"] = validation_samples
-    data["val_y"] = validation[0]
-    print "The accuracy on the validation set is: " + str(1 - perceptron_test(w, data, "val_x", "val_y"))
+    training_data_impact(raw_data)
+    toc = time.time()
+    print toc-tic
+    # training, validation = partition_data(raw_data, 4000)
+    # training = clean_data(training)
+    # validation = clean_data(validation)
+    # print "Data pre-processed"
+    # vocab = build_vocab(training[1])
+    # print "Vocabulary built"
+    # samples = build_feature_list(training[1], vocab)
+    # print "Feature matrix formed"
+    # data = {}
+    # data["train_x"] = samples
+    # data["train_y"] = training[0]
+    # w, k, itr = perceptron_train_avg(data, 1000)
+    # # print w
+    # print "The number of misclasified samples: "
+    # print k
+    # # print itr
+    # print "The error on the training set is: " + str(perceptron_test(w, data, "train_x", "train_y"))
+    # validation_samples = build_feature_list(validation[1], vocab)
+    # print "Validation samples matrix formed"
+    # data["val_x"] = validation_samples
+    # data["val_y"] = validation[0]
+    # print "The accuracy on the validation set is: " + str(1 - perceptron_test(w, data, "val_x", "val_y"))
 
-    test_data = read_data("./data/spam_test.txt")
-    test_data = clean_data(test_data)
-    test_samples = build_feature_list(test_data, vocab)
-    data["test_x"] = test_samples
-    data["test_y"] = test_data[0]
-    # print "The accuracy on test data is: " + str(1 - perceptron_test(w, data, "test_x", "test_y"))
-    most_impact, least_impact = relevant_weights(w, vocab, 15)
-    # print most_impact
-    # print least_impact
+    # test_data = read_data("./data/spam_test.txt")
+    # test_data = clean_data(test_data)
+    # test_samples = build_feature_list(test_data, vocab)
+    # data["test_x"] = test_samples
+    # data["test_y"] = test_data[0]
+    # # print "The accuracy on test data is: " + str(1 - perceptron_test(w, data, "test_x", "test_y"))
+    # most_impact, least_impact = relevant_weights(w, vocab, 15)
+    # # print most_impact
+    # # print least_impact
 if __name__ == "__main__":
     main()
